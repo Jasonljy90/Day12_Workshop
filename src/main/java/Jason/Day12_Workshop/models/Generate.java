dad616de13ce5ed519dfa91aa714f8190d09e06c
Jason Lee Jian Ying <79128355+Jasonljy90@users.noreply.github.com>
2023-01-04 16:01:43 +0800

    initial code

---
+++ src/main/java/Jason/Day12_Workshop/models/Generate.java
@@ -0,0 +1,14 @@
+package Jason.Day12_Workshop.models;
+
+public class Generate {
+    private int numberVal = 1;
+
+    public int getNumberVal() {
+        return numberVal;
+    }
+
+    public void setNumberVal(int numberVal) {
+        this.numberVal = numberVal;
+    }
+
+}
---
+++ src/test/java/Jason/Day12_Workshop/Day12WorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package Jason.Day12_Workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day12WorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
